bb584071298a55582c8d98266eefa9ecfdeb00ac
James Downer <jamesdowner@sky.com>
2023-07-26 21:31:35 +0100

    Get console app outputting

--- FizzBuzzCodeKata.App/Program.cs
+++ FizzBuzzCodeKata.App/Program.cs
@@ -1,7 +1,12 @@
 ﻿using Ardalis.GuardClauses;
+using FizzBuzzCodeKata.Core.Models;
 using FizzBuzzCodeKata.Core.Rules;
 using FizzBuzzCodeKata.Core.Services;
 using Microsoft.Extensions.DependencyInjection;
+
+// Range values as mandated by the requirements brief
+const int NumberRangeMin = 1;
+const int NumberRangeMax = 100;
 
 // Register Services with Dependency Injection Container
 var serviceProvider = new ServiceCollection()
@@ -14,6 +19,8 @@ var serviceProvider = new ServiceCollection()
     .BuildServiceProvider();
 
 var menuService = Guard.Against.Null(serviceProvider.GetService<IMenuService>());
+var fizzBuzzRuleEngine = Guard.Against.Null(serviceProvider.GetService<IFizzBuzzRuleEngine>());
+
 
 ConsoleKeyInfo keyReader;
 
@@ -27,8 +34,21 @@ do
     switch (keyReader.KeyChar.ToString())
     {
         case "1":
-            Console.WriteLine("TBC");
+            ProcessFullRangeOfValidNumbersApproach1();
             break;
     }
 }
 while (keyReader.Key != ConsoleKey.Escape);
+
+void ProcessFullRangeOfValidNumbersApproach1()
+{
+    Console.WriteLine("Rules Pattern");
+
+    var model = new FizzbuzzModel();
+
+    for (var number = NumberRangeMin; number <= NumberRangeMax; number++)
+    {
+        model.InputNumber = number;
+        Console.WriteLine(fizzBuzzRuleEngine?.ExecuteRules(model));
+    }
+}
